2be9911d4e7096e419642337994652a2ef9de8c4
dhvanit2804 <dhvanitparate123@gmail.com>
2026-03-18 15:14:26 +0530

    database connection

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,29 @@
+import User from "../models/User.js";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res
+        .status(400)
+        .json({ message: "Please enter a valid email address" });
+    }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+  } catch (error) {}
+};
