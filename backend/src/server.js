2e22e7ead3ee208f54dd0cc56d5e736f15abdc0c
dhvanit2804 <dhvanitparate123@gmail.com>
2026-06-04 16:13:30 +0530

    socket implementation

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,9 +6,8 @@ import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
-
-const app = express();
 const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
@@ -29,7 +28,7 @@ if (ENV.NODE_ENV === "production") {
   });
 }
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
