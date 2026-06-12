bd2090ecf30c877be5f6e07d8df36123b9868f9b
dhvanit2804 <dhvanitparate123@gmail.com>
2026-06-12 17:51:45 +0530

    final commit

--- backend/src/emails/emailHandlers.js
+++ backend/src/emails/emailHandlers.js
@@ -1,7 +1,13 @@
 import { resendClient, sender } from "../lib/resend.js";
+import { ENV } from "../lib/env.js";
 import { createWelcomeEmailTemplate } from "../emails/emailTemplates.js";
 
 export const sendWelcomeEmail = async (email, name, clientURL) => {
+  if (ENV.NODE_ENV !== "production") {
+    console.log("Skipping welcome email in non-production environment");
+    return null;
+  }
+
   const { data, error } = await resendClient.emails.send({
     from: `${sender.name} <${sender.email}>`,
     to: email,
